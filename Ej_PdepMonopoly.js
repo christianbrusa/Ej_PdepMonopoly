8f805294c650e3477b67f4a51a0c7e7ce658065d
christianbrusa <christianbrusa@hotmail.com>
2022-02-22 22:19:43 -0300

    Beautify code

--- Ej_PdepMonopoly.js
+++ Ej_PdepMonopoly.js
@@ -1,48 +1,39 @@
 //Idea de como se forma un Personaje
 class Personaje {
-  //Metodo constructor
-  constructor(nombre,dinero,tactica,propiedades,acciones){
-    this.nombre = nombre;
-    this.dinero = dinero;
-    this.tactica = tactica;
-    this.propiedades = propiedades;
-    this.acciones = acciones;
-  }
-  
-  pasarPorElBanco(){
-    this.dinero += 40;
-    this.tactica = "Comprador compulsivo";
-  }
-  
-  gritar(){
-  let nuevoNombre = "AHHHH" + this.nombre;
-  this.nombre = nuevoNombre;
-  }
+    //Metodo constructor
+    constructor(nombre, dinero, tactica, propiedades, acciones) {
+        this.nombre = nombre;
+        this.dinero = dinero;
+        this.tactica = tactica;
+        this.propiedades = propiedades;
+        this.acciones = acciones;
+    }
+
+    pasarPorElBanco() {
+        this.dinero += 40;
+        this.tactica = "Comprador compulsivo";
+    }
+
+    gritar() {
+        let nuevoNombre = "AHHHH" + this.nombre;
+        this.nombre = nuevoNombre;
+    }
 }
 
 //Creando personajes
 const jugador1 = new Personaje("Carolina", 500, "Accionista", {},
     function pagarAAccionistas() {
-				if(this.tactica === "Accionista"){
-           this.dinero += 200;
+        if (this.tactica === "Accionista") {
+            this.dinero += 200;
+        } else {
+            this.dinero -= 100;
         }
-  			else{
-        	 this.dinero -= 100;
-        }
-    });
+    }
+);
+
 const jugador2 = new Personaje("Manuel", 500, "Oferente singular", {},
-		function enojarse(){
-				this.dinero += 50;
-				jugador2.gritar();
-		});
-
-
-
-
-
-
-
-
-
-
-
+    function enojarse() {
+        this.dinero += 50;
+        jugador2.gritar();
+    }
+);
