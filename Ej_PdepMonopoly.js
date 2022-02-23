e6e07ad40624ffabf8e4a53a0f861c3f118d7e2b
christianbrusa <christianbrusa@hotmail.com>
2022-02-22 22:08:54 -0300

    Creación clase y constructor

---
+++ Ej_PdepMonopoly.js
@@ -0,0 +1,11 @@
+//Idea de como se forma un Personaje
+class Personaje {
+  //Metodo constructor
+  constructor(nombre,dinero,tactica,propiedades,acciones){
+    this.nombre = nombre;
+    this.dinero = dinero;
+    this.tactica = tactica;
+    this.propiedades = propiedades;
+    this.acciones = acciones;
+  }
+}
