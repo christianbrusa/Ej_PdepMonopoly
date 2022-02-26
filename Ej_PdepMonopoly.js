e964680b5aa5dd3dc7aee16dbfce081ce377c625
christianbrusa <christianbrusa>
2022-02-26 03:04:55 -0300

    Creación acción subastar()

--- Ej_PdepMonopoly.js
+++ Ej_PdepMonopoly.js
@@ -1,3 +1,6 @@
+const _ = require("lodash");
+const propiedades = require("./propiedades");
+
 //Idea de como se forma un Personaje
 class Personaje {
     //Metodo constructor
@@ -18,10 +21,21 @@ class Personaje {
         let nuevoNombre = "AHHHH" + this.nombre;
         this.nombre = nuevoNombre;
     }
+
+    subastar(propiedad) {
+        let filtro = x => x.nombre === propiedad;
+        let posicion = _.findIndex(propiedades, filtro);
+        if ((this.tactica === "Oferente singular" || this.tactica === "Accionista") &&
+            (this.dinero >= propiedades[posicion].precio) && propiedades[posicion].disponible) {
+            this.propiedades.push(propiedades[posicion]);
+            this.dinero -= propiedades[posicion].precio;
+            propiedades[posicion].disponible = false;
+        }
+    }
 }
 
 //Creando personajes
-const jugador1 = new Personaje("Carolina", 500, "Accionista", {},
+const jugador1 = new Personaje("Carolina", 500, "Accionista", [],
     function pagarAAccionistas() {
         if (this.tactica === "Accionista") {
             this.dinero += 200;
@@ -31,9 +45,17 @@ const jugador1 = new Personaje("Carolina", 500, "Accionista", {},
     }
 );
 
-const jugador2 = new Personaje("Manuel", 500, "Oferente singular", {},
+const jugador2 = new Personaje("Manuel", 500, "Oferente singular", [],
     function enojarse() {
         this.dinero += 50;
         jugador2.gritar();
     }
 );
+
+
+jugador1.subastar("El Muelle")
+jugador1.subastar("Avenida Vermont")
+jugador1.subastar("Avenida Nueva York")
+console.log(jugador1)
+jugador2.subastar("Avenida Estados")
+console.log(jugador2)
