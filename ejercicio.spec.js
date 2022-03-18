9b3520218a7139ed1ad11966bc5bba9615d5d3a5
christianbrusa <christianbrusa@hotmail.com>
2022-03-18 01:37:15 -0300

    Creación Test función enojarse()

--- ejercicio.spec.js
+++ ejercicio.spec.js
@@ -13,7 +13,7 @@ const {
 require("should");
 
 const jugador1 = new Personaje("Carolina", 500, "Accionista", [], [pasarPorElBanco]);
-const jugador2 = new Personaje("Manuel", 500, "Oferente singular", [], [pasarPorElBanco]);
+const jugador2 = new Personaje("Manuel", 500, "Oferente singular", [], []);
 
 const propiedad1 = new Propiedad("Avenida Mediterráneo", 60, true);
 const propiedad2 = new Propiedad("Avenida Báltica", 60, true);
@@ -34,7 +34,7 @@ const propiedad14 = new Propiedad("El Muelle", 400, true);
 describe("Test", () => {
     describe("Pasar por el banco", () => {
         it("Deberia aumentar $40 y cambiar la tactica del pj", () => {
-            if (jugador1.acciones.includes(pasarPorElBanco)) {
+            if (jugador1.acciones.includes(pasarPorElBanco) && jugador1.acciones.length <= 1) {
                 pasarPorElBanco.ejecutar(jugador1)
                 jugador1.should.match({
                     nombre: "Carolina",
@@ -50,7 +50,7 @@ describe("Test", () => {
                     }]
                 })
             }
-            if (jugador2.acciones.includes(pasarPorElBanco)) {
+            if (jugador2.acciones.includes(pasarPorElBanco) && jugador1.acciones.length <= 1) {
                 pasarPorElBanco.ejecutar(jugador2)
                 jugador2.should.match({
                     nombre: "Manuel",
@@ -62,6 +62,87 @@ describe("Test", () => {
                         ejecutar: function ejecutar(jugador, propiedadSubastar, propiedadComprar) {
                             jugador.dinero += this.valorFijoPorRetirar;
                             jugador.tactica = "Comprador compulsivo";
+                        }
+                    }]
+                })
+            }
+        });
+    });
+
+
+    describe("El jugador se enoja", () => {
+        it("Deberia aumentar $50 y agregar gritar a sus acciones", () => {
+            if (jugador1.acciones.includes(enojarse) && jugador1.acciones.includes(pasarPorElBanco)) {
+                enojarse.ejecutar(jugador1)
+                should(jugador1).match({
+                    //jugador1.should.be.eql({ me devolvió error al comparar los objetos
+                    nombre: "AHHHHCarolina",
+                    dinero: 590,
+                    tactica: "Comprador compulsivo",
+                    propiedades: [],
+                    acciones: [{
+                        valorFijoPorRetirar: 40,
+                        ejecutar: function ejecutar(jugador, propiedadSubastar, propiedadComprar) {
+                            jugador.dinero += this.valorFijoPorRetirar;
+                            jugador.tactica = "Comprador compulsivo";
+                        }
+                    }, {
+                        valorFijoSumar: 50,
+                        ejecutar: function(jugador, propiedadSubastar, propiedadComprar) {
+                            jugador.dinero += this.valorFijoSumar;
+                        }
+                    }]
+                })
+            }
+            //if (jugador1.acciones.includes(enojarse) && !jugador1.acciones.includes(pasarPorElBanco)) {
+            else if (jugador1.acciones.includes(enojarse) && jugador1.acciones.length <= 1) {
+                enojarse.ejecutar(jugador1)
+                should(jugador1).match({
+                    //jugador1.should.be.eql({ me devolvió error al comparar los objetos
+                    nombre: "AHHHHCarolina",
+                    dinero: 550,
+                    tactica: "Accionista",
+                    propiedades: [],
+                    acciones: [{
+                        valorFijoSumar: 50,
+                        ejecutar: function(jugador, propiedadSubastar, propiedadComprar) {
+                            jugador.dinero += this.valorFijoSumar;
+                        }
+                    }]
+                })
+            } else if (jugador2.acciones.includes(enojarse) && jugador2.acciones.includes(pasarPorElBanco)) {
+                enojarse.ejecutar(jugador2)
+                should(jugador2).match({
+                    //jugador1.should.be.eql({ me devolvió error al comparar los objetos
+                    nombre: "AHHHHManuel",
+                    dinero: 590,
+                    tactica: "Comprador compulsivo",
+                    propiedades: [],
+                    acciones: [{
+                        valorFijoPorRetirar: 40,
+                        ejecutar: function ejecutar(jugador, propiedadSubastar, propiedadComprar) {
+                            jugador.dinero += this.valorFijoPorRetirar;
+                            jugador.tactica = "Comprador compulsivo";
+                        }
+                    }, {
+                        valorFijoSumar: 50,
+                        ejecutar: function(jugador, propiedadSubastar, propiedadComprar) {
+                            jugador.dinero += this.valorFijoSumar;
+                        }
+                    }]
+                })
+            } else if (jugador2.acciones.includes(enojarse) && jugador2.acciones.length <= 1) {
+                enojarse.ejecutar(jugador2)
+                should(jugador2).match({
+                    //jugador1.should.be.eql({ me devolvió error al comparar los objetos
+                    nombre: "AHHHHManuel",
+                    dinero: 550,
+                    tactica: "Oferente singular",
+                    propiedades: [],
+                    acciones: [{
+                        valorFijoSumar: 50,
+                        ejecutar: function(jugador, propiedadSubastar, propiedadComprar) {
+                            jugador.dinero += this.valorFijoSumar;
                         }
                     }]
                 })
